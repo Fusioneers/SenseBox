c8ca87a65989942f3f1f4012fb765fa26a1e81a1
Daniel Meiborg <79795628+DanielMeiborg@users.noreply.github.com>
2022-03-25 15:31:10 +0100

    added model.py

--- SenseBox/evaluation/model.py
+++ SenseBox/evaluation/model.py
@@ -5,13 +5,19 @@ from keras import layers
 def create_model(input_shape, output_shape):
     """
     Creates a model with the given input and output shape.
+
+    Args:
+        input_shape: The shape of the input data.
+        output_shape: The shape of the output data.
+
     """
     model = keras.Sequential()
     model.add(layers.Dense(units=100, activation='relu',
                            input_shape=input_shape))
-    model.add(layers.Dense(units=50, activation='relu'))
+    model.add(layers.Dense(units=100, activation='relu'))
+    model.add(layers.Dense(units=100, activation='relu'))
+    model.add(layers.Dense(units=100, activation='relu'))
     model.add(layers.Dense(units=output_shape, activation='linear'))
 
-    print(model.summary())
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
