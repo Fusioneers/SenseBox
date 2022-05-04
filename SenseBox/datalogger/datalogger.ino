b0fd361001f139620212751b44c64071642e7244
Paul Maier <pauljustus279@gmail.com>
2022-05-04 16:24:34 +0200

    Updated datalogger and new data and moved python files back into place

--- SenseBox/datalogger/datalogger.ino
+++ SenseBox/datalogger/datalogger.ino
@@ -22,7 +22,7 @@ uint32_t syncTime = 0; // Zeit der letzten Synchronisierung
 #define onOffButton A2
 
 // Der analoge Port, an dem der Hallsensor angeschlossen ist
-#define hallSensor A3
+#define hallSensor 2
 
 // Der analoge Port, an dem der UV-Sensor angeschlossen ist
 #define UVSensor A0
@@ -30,19 +30,14 @@ uint32_t syncTime = 0; // Zeit der letzten Synchronisierung
 // Kontrolliert wann die Daten gesammelt werden -> kann über den Knopf an onOffButton kontrolliert werden
 int _running = 0;
 
-// Speichert wann der Hall-Sensor des Anemometers das letzte Mal ausgelöst wurde
-int lastRotation = 0;
-
-// Speichert wie lange das Anemometer für die letzte Umdrehug gebraucht hat
-int rotationTime = 0;
-
 // Zeitpunkt, zu dem der Start-Konopf gedrückt wurde -> läuft nach ca. 50 Tagen über
 unsigned long runningSince = 0; 
 
 // Port des SD-Karten-Lesemoduls
 const int chipSelect = 10;
 
-File logfile; // Instanziert das Log-File
+File logfile; // Instanziert das allgemeine Log-File
+File hall_trigger_logfile; // Instanziert das Log-File für die Auslösungen des Hall-Sensors
 
 // Falls ein Fehler auftritt, wird er von dieser Funktion gehandhabt
 void error(String message) {
@@ -54,10 +49,11 @@ void error(String message) {
   #endif //ECHO_TO_SERIAL
 }
 
+
 void hallSensorTriggered() {
-  int _now = millis();
-  rotationTime = _now - lastRotation;
-  lastRotation = _now;
+  if (_running) {
+    hall_trigger_logfile.println(millis());
+  }
 }
 
 void setup(void)
@@ -65,9 +61,6 @@ void setup(void)
   // Kontrolliert ob ein Fehler innerhalb des setup Teils aufgetreten ist
   bool errorOccuredInSetup = false;
 
-  // Attached die Funktion hallSensorTriggered, zu einem Ändern der Voltage am Port hallSensor von HIGH zu LOW
-  attachInterrupt(digitalPinToInterrupt(hallSensor), hallSensorTriggered, FALLING);
-  
   // Initialisiert den seriellen Monitor
   Serial.begin(9600);
   
@@ -81,11 +74,12 @@ void setup(void)
 
   // Setzt den Pin-Mode des Hallsesnors auf INPUT
   pinMode(hallSensor, INPUT);
+  
+  // Attached die Funktion hallSensorTriggered, zu einem Ändern der Voltage am Port hallSensor von HIGH zu LOW
+  attachInterrupt(digitalPinToInterrupt(hallSensor), hallSensorTriggered, FALLING);
 
   // Die gelbe LED wird für den restlichen setup-Teil angeschaltet
   digitalWrite(yellowLED, HIGH); 
-
-  delay(1000); // Warte kurz
 
   Serial.println("Initializing SD card...");
   // Setzt den SD-Karten Pin vorsichtshalber bereits auf OUTPUT
@@ -111,14 +105,32 @@ void setup(void)
     }
   }
 
+  // Erstellt ein neues Log-File mit dem Namen HALLOG00 + zweistellige noch nicht verwendete Zahl
+  char hall_trigger_filename[] = "HALLOG00.CSV";
+  // Loopt duch alle möglichen Namen
+  for (uint8_t i = 0; i < 100; i++) {
+    hall_trigger_filename[6] = i / 10 + '0';
+    hall_trigger_filename[7] = i % 10 + '0';
+    if (! SD.exists(hall_trigger_filename)) {
+      // Falls der Name noch nicht belegt ist, wird das Log-File erstellt und der Loop unterbrochen
+      hall_trigger_logfile = SD.open(hall_trigger_filename, FILE_WRITE);
+      break;
+    }
+  }
+
   // Falls keine Log-Datei erstellt wurde (weil z.B. alle Namen belegt waren), wird eine Fehlermeldung ausgegeben
-  if (! logfile) {
+  if (!logfile) {
     error("Could not create a logfile");
+    errorOccuredInSetup = true;
+  } else if (!hall_trigger_logfile) {
+    error("Could not create a logfile for the hall sensor triggers");
     errorOccuredInSetup = true;
   } else {
     // Andernfalls wird der Name der erstellten Log-Datei ausgegeben
     Serial.print("Logging to: ");
-    Serial.println(filename);
+    Serial.print(filename);
+    Serial.print(" and ");
+    Serial.println(hall_trigger_filename);
   }
 
   // Wenn keine Daten vom BME abgefragt werden können ...
@@ -126,13 +138,14 @@ void setup(void)
     error("Could not read data from BME"); // ... dann soll eine Fehlermeldung ausgegeben werden.
   }
 
-  // Setzt die Spaltennamen für die CSV-Log-Datei
-  logfile.println("timestamp,temperature,pressure,humidity,altitude,hallsensor,uv"); 
+  // Setzt die Spaltennamen für die CSV-Log-Dateien
+  logfile.println("timestamp,temperature,pressure,humidity,altitude,uv");
+  hall_trigger_logfile.println("timestamp");
   #if ECHO_TO_SERIAL
     // Gibt eine Warnung aus
     Serial.println("Note: The timestamp starts with the execution of the programm and will overflow after approx. 50 days.");
     // Setzt die Spaltennamen für die Ausgabe
-    Serial.println("timestamp,temperature,pressure,humidity,altitude,hallsensor,uv"); 
+    Serial.println("timestamp,temperature,pressure,humidity,altitude,uv"); 
   #endif //ECHO_TO_SERIAL
 
     // Falls noch keine Fehler aufgetreten ist, wird auf das Drücken des Start-Knopfs gewartet
@@ -167,6 +180,12 @@ void loop(void)
   // Überprüft, ob der An-/Aus-Knopf gedrückt wird, falls ja, wird das Data-Logging beendet
   if (analogRead(onOffButton) >= 1023) {
     _running = 0;
+    // Lässt die gelbe LED blinken, um zu signalisieren, dass synchronisiert wird
+    digitalWrite(yellowLED, HIGH);
+    logfile.flush();
+    hall_trigger_logfile.flush();
+    digitalWrite(yellowLED, LOW);
+    
     digitalWrite(greenLED, LOW); // Schaltet die grüne LED aus
     digitalWrite(yellowLED, HIGH); // Schaltet die gelbe LED an
     digitalWrite(redLED, HIGH); // Schaltet die rote LED an
@@ -187,9 +206,6 @@ void loop(void)
     int hum = bme280.getHumidity(); // Luftfeuchtigkeit
     double alt = bme280.calcAltitude(pres); // Berechnete Höhe
 
-    // Liest die Sensorwerte des Hall-Sensors aus
-    int hall = analogRead(hallSensor);
-
     // Liest die Sensorwerte des UV-Sensors aus
     float analogSignal = analogRead(UVSensor);
     float voltage = analogSignal/1023*5;
@@ -205,8 +221,6 @@ void loop(void)
     logfile.print(", ");
     logfile.print(alt);
     logfile.print(", ");
-    logfile.print(hall);
-    logfile.print(", ");
     logfile.print(uvIndex);
     #if ECHO_TO_SERIAL
       Serial.print(", ");
@@ -220,8 +234,6 @@ void loop(void)
       Serial.print(", ");
       Serial.print(alt);
       Serial.print("m");
-      Serial.print(", ");
-      Serial.print(hall);
       Serial.print(", ");
       Serial.print(uvIndex);
     #endif //ECHO_TO_SERIAL
@@ -238,6 +250,7 @@ void loop(void)
     // Lässt die gelbe LED blinken, um zu signalisieren, dass synchronisiert wird
     digitalWrite(yellowLED, HIGH);
     logfile.flush();
+    hall_trigger_logfile.flush();
     digitalWrite(yellowLED, LOW);
   }
 }
