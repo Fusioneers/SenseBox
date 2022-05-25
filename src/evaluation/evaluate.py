fd6cbc91da0264e4e1fb6a0b53861c1a51687d19
DanielMeiborg <mail@danielmeiborg.com>
2022-05-25 16:17:30 +0200

    added plot

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,13 +1,18 @@
+import os
+
 import keras
 import numpy as np
 import pandas
 import pandas as pd
+from tensorflow import metrics
 
 import data_preparation
 import joblib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tensorflow_addons import activations
+
+from src.evaluation import constants
 
 
 def convert_altitude_to_pressure(altitude: np.ndarray) -> np.ndarray:
@@ -31,26 +36,51 @@ def evaluate_csv(model_path,
     out = model.predict(X_dataset)
     out = y_scaler.inverse_transform(out)
 
-    # sns.pairplot(pd.read_csv(logger_path))
-    # plt.show()
-
     return out
 
 
-def main():
-    model_path = '/home/daniel/coding/SenseBox/src/evaluation/models/model_5_epochs_2022-05-20 15-12-21.352063.tf'
+def plot(path, X_test, y_test):
+    model_path = path  # '/home/daniel/coding/SenseBox/src/evaluation/models/model_2022-05-20 21-40-10.523628.h5'
     X_scaler_path = '/home/daniel/coding/SenseBox/src/evaluation/X_scaler.bin'
     y_scaler_path = '/home/daniel/coding/SenseBox/src/evaluation/y_scaler.bin'
     logger_path = '/home/daniel/coding/SenseBox/data/2022-05-13_Messreihe_Zug_S1_Hbf_Kirchzarten/LOGGER03.CSV'
     hallog_path = '/home/daniel/coding/SenseBox/data/2022-05-13_Messreihe_Zug_S1_Hbf_Kirchzarten/HALLOG03.CSV'
+    model = keras.models.load_model(model_path)
+    y_scaler = joblib.load(y_scaler_path)
+
     out = evaluate_csv(model_path,
                        X_scaler_path,
                        y_scaler_path,
                        logger_path,
                        hallog_path)
     print(pd.DataFrame(out, columns=['altitude']).describe())
-    plt.plot(out)
+    data = pd.read_csv(logger_path)
+    plt.plot(data['timestamp'], out)
+    plt.title(path.split('/')[-1])
     plt.show()
+
+    y_pred = model.predict(X_test)
+    error = metrics.mean_absolute_error(
+        y_scaler.inverse_transform(y_test[..., np.newaxis]).squeeze(),
+        y_scaler.inverse_transform(y_pred).squeeze())
+
+    return error
+
+
+def main():
+    (X_train, X_test, y_train, y_test,
+     columns, X_scaler, y_scaler) = data_preparation.create_dataset()
+    errors = []
+
+    for path in os.scandir(constants.get_path('src', 'evaluation', 'models')):
+        if path.is_file() and 'h5' in path.name:
+            try:
+                error = plot(path.path, X_test, y_test)
+                errors.append([error, path.name])
+            except Exception as e:
+                print(e)
+    print(f'Errors: {errors}')
+    print(f'Min: {min(errors)}')
 
 
 if __name__ == '__main__':
